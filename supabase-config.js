b6425bed9d2c0a1d0f942c330600e9093406ebe0
amstrongeek <louisspain45@gmail.com>
2026-06-19 11:15:59 +0200

    Configure Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.RIP_SUPABASE = {
-  url: "",
+  url: "https://kuyeewovrycmmxbryytt.supabase.co/rest/v1/",
   // Colle ici la cle publique anon/publishable. Ne colle jamais service_role ou secret.
-  anonKey: ""
+  anonKey: "sb_publishable_vSkki9LFsTrNSzmpRmjhCw_my7UGLUZ"
 };
